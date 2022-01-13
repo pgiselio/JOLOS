e2a32196a3468c10b7bb93831d471ed48a83ba15
Pedro Gisélio <pgiselio@hotmail.com>
2022-01-13 14:08:56 -0300

    removido coiso q só funciona no mobile

--- js/logado.js
+++ js/logado.js
@@ -1,14 +1,8 @@
 const btnCollpseSidemenu = document.getElementById('btn-collapse-sidemenu');
 const btnHam = document.querySelector('.botao-ham');
-const mainContent = document.querySelector('.main');
 const bodyDOM = document.getElementsByTagName("body")[0];
 
 btnCollpseSidemenu.addEventListener('click', () =>{
     bodyDOM.classList.toggle('expand-sidemenu');
     btnHam.classList.toggle('active');
-});
-
-mainContent.addEventListener('click', () =>{
-    bodyDOM.classList.remove('expand-sidemenu');
-    btnHam.classList.remove('active'); 
 });
