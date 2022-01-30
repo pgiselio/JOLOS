91a582354bf2ed995154278a4e95567426cf8930
Pedro Gisélio <pgiselio@hotmail.com>
2022-01-30 18:31:15 -0300

    corrigido bug na animação da sidebar

--- js/logado-sidebar.js
+++ js/logado-sidebar.js
@@ -2,6 +2,8 @@ const btnCollpseSidemenu = document.getElementById('btn-collapse-sidemenu');
 const btnHam = document.querySelector('.botao-ham');
 const bodyDOM = document.getElementsByTagName("body")[0];
 
+bodyDOM.classList.add("remove-transbar");
+
 btnCollpseSidemenu.addEventListener('click', () => {
     bodyDOM.classList.toggle('toggle-sidemenu');
     btnHam.classList.toggle('active');
@@ -23,11 +25,12 @@ function populateStorage() {
 }
 function checkSidebarState() {
     const mq = window.matchMedia("(min-width: 766px)");
-
     if (mq.matches && localStorage.getItem('sidebar-collapsed') == ('collapsed')) {
         bodyDOM.classList.add('toggle-sidemenu');
         btnHam.classList.add('active');
-    }
+    }  
 }
 
-
+setTimeout(() => {
+    bodyDOM.classList.remove("remove-transbar");
+}, 600);  
