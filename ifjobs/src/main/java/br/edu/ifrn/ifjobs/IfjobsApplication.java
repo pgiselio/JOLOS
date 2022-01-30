fd65db06c28ad4954925a222531e0fc6814bb0bc
Pedro Gisélio <pgiselio@hotmail.com>
2022-01-30 19:29:00 -0300

    Change one

---
+++ ifjobs/src/main/java/br/edu/ifrn/ifjobs/IfjobsApplication.java
@@ -0,0 +1,13 @@
+package br.edu.ifrn.ifjobs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IfjobsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IfjobsApplication.class, args);
+	}
+
+}
---
+++ ifjobs/src/test/java/br/edu/ifrn/ifjobs/IfjobsApplicationTests.java
@@ -0,0 +1,13 @@
+package br.edu.ifrn.ifjobs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IfjobsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
