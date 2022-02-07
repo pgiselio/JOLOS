ae690699ee744e5dc63ad0c4ac95d99587f72403
Lucas Jdev <lucas.jdev1@gmail.com>
2022-02-07 01:13:48 -0300

    campos dto e query para cpf

--- src/main/java/br/edu/ifrn/ifjobs/dto/usuario/UsuarioInsertDTO.java
+++ src/main/java/br/edu/ifrn/ifjobs/dto/usuario/UsuarioInsertDTO.java
@@ -1,23 +1,41 @@
 package br.edu.ifrn.ifjobs.dto.usuario;
 
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+
+import org.hibernate.validator.constraints.br.CPF;
 import org.modelmapper.ModelMapper;
-import org.springframework.beans.factory.annotation.Autowired;
 
 import br.edu.ifrn.ifjobs.dto.Dto;
 import br.edu.ifrn.ifjobs.model.Usuario;
 
 public class UsuarioInsertDTO implements Dto<Usuario, UsuarioInsertDTO> {
 
-    @Autowired
+    @NotBlank(message = "Nome não informado!!")
+    private String nome;
+
+    @Email(message = "Formato de email inválido!!")
+    @NotBlank(message = "Email não informado!!")
+    private String email;
+
+    @NotBlank(message = "não informada")
+    private String senha;
+
+    @NotBlank(message = "CPF não informado!!")
+    @CPF(message = "Formato de CPF inválido!!")
+    private String cpf;
+
     private ModelMapper modelMapper;
 
     @Override
     public Usuario convertToEntity() {
+        modelMapper = new ModelMapper();
         return modelMapper.map(this, Usuario.class);
     }
 
     @Override
     public UsuarioInsertDTO convertToDto(Usuario entity) {
+        modelMapper = new ModelMapper();
         return modelMapper.map(entity, UsuarioInsertDTO.class);
     }
 
--- src/main/java/br/edu/ifrn/ifjobs/repository/AlunoRespository.java
+++ src/main/java/br/edu/ifrn/ifjobs/repository/AlunoRespository.java
@@ -1,11 +1,18 @@
 package br.edu.ifrn.ifjobs.repository;
 
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import br.edu.ifrn.ifjobs.model.Aluno;
 
 @Repository
-public interface AlunoRespository extends CrudRepository<Aluno, Integer> {
+public interface AlunoRespository extends JpaRepository<Aluno, Integer> {
 
+    @Query(value = """
+                SELECT id
+                FROM aluno a
+                WHERE a.cpf = ?1
+            """, nativeQuery = true)
+    public int findIdByCpf(String cpf);
 }
