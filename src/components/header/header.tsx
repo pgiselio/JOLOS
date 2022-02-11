260bf668922a0d9b37b00c7f8b8ce136d71efa20
Pedro Gisélio <pgiselio@hotmail.com>
2022-02-11 16:35:31 -0300

    testando coisos

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -1,37 +1,41 @@
-function Collapse(){
+function Collapse() {
   document.body.classList.toggle('toggle-sidemenu');
+  storageSidebarState();
+}
+
+function storageSidebarState() {
   if (document.body.classList.contains('toggle-sidemenu')) {
-        localStorage.setItem('sidebar-collapsed', 'collapsed');
+    localStorage.setItem('sidebar-collapsed', 'collapsed');
   } else {
-        localStorage.setItem('sidebar-collapsed', '');
+    localStorage.setItem('sidebar-collapsed', '');
   }
 }
 export function Header() {
-    return(
-        <header className="header">
-          <nav className="navigate">
-            <div className="menu-container">
-              <button id="btn-collapse-sidemenu" onClick={Collapse} aria-label="Botão de esconder ou mostrar menu lateral">
-                <div className="three-bars-btn">
-                  <div className="lines-btn">
-                    <span></span>
-                    <span></span>
-                    <span></span>
-                  </div>
-                </div>
-                <div className="botao-ham">
-                  <span></span>
-                  <span></span>
-                  <span></span>
-                </div>
-              </button>
-              <img src="../images/logo.svg" className="logo" alt="logo projeto" />
-              <button className="btn-notify" aria-label="Botão de notificações">
-                <i className="fas fa-bell"></i>
+  return (
+    <header className="header">
+      <nav className="navigate">
+        <div className="menu-container">
+          <button id="btn-collapse-sidemenu" onClick={Collapse} aria-label="Botão de esconder ou mostrar menu lateral">
+            <div className="three-bars-btn">
+              <div className="lines-btn">
                 <span></span>
-              </button>
+                <span></span>
+                <span></span>
+              </div>
             </div>
-          </nav>
-        </header>
-    );
+            <div className="botao-ham">
+              <span></span>
+              <span></span>
+              <span></span>
+            </div>
+          </button>
+          <img src="../images/logo.svg" className="logo" alt="logo projeto" />
+          <button className="btn-notify" aria-label="Botão de notificações">
+            <i className="fas fa-bell"></i>
+            <span></span>
+          </button>
+        </div>
+      </nav>
+    </header>
+  );
 }
---
+++ src/landingpage/navbar.tsx
@@ -0,0 +1,33 @@
+export function LandNavBar() {
+    return(
+        <header className="navigate-container">
+                <nav className="navigate">
+                    <div className="menu-container">
+                        <a href="/" className="logo-nav"><img src="images/logo.svg" alt="" /></a>
+                        <ul className="menu">
+                            <li><a href="#1">Tadah</a></li>
+                            <li><a href="#2">Cursos</a> </li>
+                            <li><a href="#3">Sobre</a> </li>
+                            <li><a href="#4">Contato</a> </li>
+                        </ul>
+                        <div className="acesso">
+                            <a href="pages/login.html" className="login-bt">Login</a>
+                            <a href="pages/signup.html" className="signup-bt">Cadastro</a>
+                        </div>
+                        <div className="mobile-access">
+                            <a href="#" className="access-bt"></a>
+                            <div className="acesso-mobile">
+                                <a href="pages/login.html" className="login-bt">Login</a>
+                                <a href="pages/signup.html" className="signup-bt">Cadastro</a>
+                            </div>
+                            <div id="botao-ham" className="botao-ham">
+                                <span></span>
+                                <span></span>
+                                <span></span>
+                            </div>
+                        </div>
+                    </div>
+                </nav>
+            </header>
+    );
+}
