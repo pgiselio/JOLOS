137bf74cd195f0825164caf859c51865465760ad
Lucas Jdev <lucas.jdev1@gmail.com>
2022-02-12 09:25:56 -0300

    base do service

--- src/main/java/br/edu/ifrn/ifjobs/service/EmpresaService.java
+++ src/main/java/br/edu/ifrn/ifjobs/service/EmpresaService.java
@@ -1,6 +1,12 @@
 package br.edu.ifrn.ifjobs.service;
 
+import org.springframework.stereotype.Service;
+
+import br.edu.ifrn.ifjobs.repository.EmpresaRepository;
+
+@Service
 public class EmpresaService {
 
-    private Empresa
+    private EmpresaRepository repository;
+
 }
