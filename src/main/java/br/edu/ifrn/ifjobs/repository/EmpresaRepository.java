5514935245aaba524693e68423867f76283b8dc0
Lucas Jdev <lucas.jdev1@gmail.com>
2022-02-12 09:34:02 -0300

    anotações

--- src/main/java/br/edu/ifrn/ifjobs/repository/EmpresaRepository.java
+++ src/main/java/br/edu/ifrn/ifjobs/repository/EmpresaRepository.java
@@ -1,9 +1,11 @@
 package br.edu.ifrn.ifjobs.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import br.edu.ifrn.ifjobs.model.Empresa;
 
+@Repository
 public interface EmpresaRepository extends JpaRepository<Empresa, Integer> {
 
 }
