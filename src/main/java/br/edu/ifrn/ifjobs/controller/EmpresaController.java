b87a83e2c006e216df6974f5e2bc3ce4e8ff70ea
Lucas Jdev <lucas.jdev1@gmail.com>
2022-02-13 13:00:52 -0300

    iniciando o controller da empresa

---
+++ src/main/java/br/edu/ifrn/ifjobs/controller/EmpresaController.java
@@ -0,0 +1,10 @@
+package br.edu.ifrn.ifjobs.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(path = "/empresa")
+public class EmpresaController {
+
+}
