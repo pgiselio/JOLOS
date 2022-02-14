beaa6df5adaea7d0961a70f039289cc88a9b60a8
Pedro Gisélio <pgiselio@hotmail.com>
2022-02-13 22:46:27 -0300

    login kkkkk

--- src/pages/login/login.tsx
+++ src/pages/login/login.tsx
@@ -1,5 +1,6 @@
 import "./login.scss";
 export function LoginPage() {
+    document.body.style.background = "linear-gradient(45deg, rgba(6,52,15,1) 0%, rgba(28,136,50,1) 50%, rgba(147,255,169,1) 100%), rgb(6,52,15)";
     return (
         <div id="login-page">
             <main>
