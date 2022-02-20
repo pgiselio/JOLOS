9b4c103af6e459d6fb406690c0b1fc069e3cb89e
Lucas Jdev <lucas.jdev1@gmail.com>
2022-02-20 16:51:26 -0300

    criação do método buscaTodos para EmpresaController

--- src/main/java/br/edu/ifrn/ifjobs/controller/EmpresaController.java
+++ src/main/java/br/edu/ifrn/ifjobs/controller/EmpresaController.java
@@ -1,10 +1,28 @@
 package br.edu.ifrn.ifjobs.controller;
 
+import java.util.List;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import br.edu.ifrn.ifjobs.model.Empresa;
+import br.edu.ifrn.ifjobs.service.EmpresaService;
 
 @RestController
 @RequestMapping(path = "/empresa")
 public class EmpresaController {
+
+    private EmpresaService empresaService;
+
+    @GetMapping
+    @ResponseBody
+    public ResponseEntity<List<Empresa>> buscaTodos() {
+        List<Empresa> empresas;
+        empresas = empresaService.getAll();
+        return ResponseEntity.ok(empresas);
+    }
 
 }
