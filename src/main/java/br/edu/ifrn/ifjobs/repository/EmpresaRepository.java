b9d2e8524eb21ca7c1cd3ab6d239051ff7228671
Lucas Jdev <lucas.jdev1@gmail.com>
2022-02-20 16:31:31 -0300

    criação de query para buscar empresa pelo cnpj

--- src/main/java/br/edu/ifrn/ifjobs/repository/EmpresaRepository.java
+++ src/main/java/br/edu/ifrn/ifjobs/repository/EmpresaRepository.java
@@ -1,11 +1,19 @@
 package br.edu.ifrn.ifjobs.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
+import br.edu.ifrn.ifjobs.model.Aluno;
 import br.edu.ifrn.ifjobs.model.Empresa;
 
 @Repository
 public interface EmpresaRepository extends JpaRepository<Empresa, Integer> {
 
+    @Query(value = """
+            SELECT *
+            FROM Empresa e
+            WHERE e.cnpj = ?1
+            """)
+    public Aluno findByCnpj(String cnpj);
 }
