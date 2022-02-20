533f9c4378dd0d150260f2c80a3b5c3f60818de1
Lucas Jdev <lucas.jdev1@gmail.com>
2022-02-20 17:21:45 -0300

    criação do método buscaPorId em EmpresaController

--- src/main/java/br/edu/ifrn/ifjobs/controller/EmpresaController.java
+++ src/main/java/br/edu/ifrn/ifjobs/controller/EmpresaController.java
@@ -7,6 +7,7 @@ import javax.validation.Valid;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -16,6 +17,7 @@ import org.springframework.web.server.ResponseStatusException;
 
 import br.edu.ifrn.ifjobs.dto.empresa.EmpresaInsertDTO;
 import br.edu.ifrn.ifjobs.exception.EmpresaNaoCadastradaException;
+import br.edu.ifrn.ifjobs.exception.EmpresaNaoEncontradaException;
 import br.edu.ifrn.ifjobs.model.Empresa;
 import br.edu.ifrn.ifjobs.service.EmpresaService;
 
@@ -38,6 +40,20 @@ public class EmpresaController {
         }
 
         return new ResponseEntity<>(empresaSalva, HttpStatus.CREATED);
+    }
+
+    @GetMapping("/{id}")
+    @ResponseBody
+    public ResponseEntity<Empresa> buscaPorId(@PathVariable(name = "id") int id) {
+        Empresa empresa;
+
+        try {
+            empresa = empresaService.getById(id);
+        } catch (EmpresaNaoEncontradaException e) {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, e.getMessage());
+        }
+
+        return ResponseEntity.ok(empresa);
     }
 
     @GetMapping
