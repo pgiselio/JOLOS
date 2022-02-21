d5907773a661f5526f260e303ee5835eb6c1ccf0
Lucas Jdev <lucas.jdev1@gmail.com>
2022-02-21 11:56:53 -0300

    autorização default

--- src/main/java/br/edu/ifrn/ifjobs/controller/UsuarioController.java
+++ src/main/java/br/edu/ifrn/ifjobs/controller/UsuarioController.java
@@ -29,6 +29,7 @@ import br.edu.ifrn.ifjobs.dto.usuario.UsuarioLoginGetDTO;
 import br.edu.ifrn.ifjobs.exception.UsuarioNaoCadastradoException;
 import br.edu.ifrn.ifjobs.exception.UsuarioNaoEncontradoException;
 import br.edu.ifrn.ifjobs.model.Email;
+import br.edu.ifrn.ifjobs.model.Role;
 import br.edu.ifrn.ifjobs.model.Usuario;
 import br.edu.ifrn.ifjobs.model.enums.StatusUsuario;
 import br.edu.ifrn.ifjobs.service.EmailService;
@@ -52,6 +53,10 @@ public class UsuarioController {
         BCryptPasswordEncoder ciptografo = new BCryptPasswordEncoder();
         usuario.setSenha(ciptografo.encode(usuario.getSenha()));
         usuario.setStatus(StatusUsuario.PENDENTE);
+
+        var papelDeAutorizacao = new Role();
+        papelDeAutorizacao.setNomeRole("Usuario");
+        usuario.addRole(papelDeAutorizacao);
 
         Usuario usuarioSalvo;
 
