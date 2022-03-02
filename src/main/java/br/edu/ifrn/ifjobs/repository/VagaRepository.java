14e7a0a332d4cd4b51343a6e14673157b34a8c33
Lucas Jdev <lucas.jdev1@gmail.com>
2022-03-02 19:21:43 -0300

    criação de repositório de vaga

---
+++ src/main/java/br/edu/ifrn/ifjobs/repository/VagaRepository.java
@@ -0,0 +1,9 @@
+package br.edu.ifrn.ifjobs.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import br.edu.ifrn.ifjobs.model.Vaga;
+
+public interface VagaRepository extends JpaRepository<Vaga, Integer> {
+
+}
