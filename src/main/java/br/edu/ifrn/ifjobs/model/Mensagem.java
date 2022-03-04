0e7ff1bbbae296c96fd739d6b483c5cd1b36e32a
Lucas Jdev <lucas.jdev1@gmail.com>
2022-03-03 22:09:23 -0300

    criação modelo de mensagem para fórum

---
+++ src/main/java/br/edu/ifrn/ifjobs/model/Mensagem.java
@@ -0,0 +1,72 @@
+package br.edu.ifrn.ifjobs.model;
+
+import java.io.Serializable;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Mensagem implements Serializable {
+
+    private static final long serialVersionUID = 12312L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @Column(nullable = false)
+    private String texto;
+
+    @Column(nullable = false)
+    private Vaga vaga;
+
+    @Column(nullable = false)
+    private Usuario usuario;
+
+    public Mensagem() {
+
+    }
+
+    public Mensagem(int id, String texto, Vaga vaga, Usuario usuario) {
+        this.id = id;
+        this.texto = texto;
+        this.vaga = vaga;
+        this.usuario = usuario;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTexto() {
+        return texto;
+    }
+
+    public void setTexto(String texto) {
+        this.texto = texto;
+    }
+
+    public Vaga getVaga() {
+        return vaga;
+    }
+
+    public void setVaga(Vaga vaga) {
+        this.vaga = vaga;
+    }
+
+    public Usuario getUsuario() {
+        return usuario;
+    }
+
+    public void setUsuario(Usuario usuario) {
+        this.usuario = usuario;
+    }
+
+}
