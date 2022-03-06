557c36b8734d6a74d3ca9c847961c773c27834fa
Lucas Jdev <lucas.jdev1@gmail.com>
2022-03-06 12:21:36 -0300

    inserção da anotação repository

--- src/main/java/br/edu/ifrn/ifjobs/repository/RoleRepository.java
+++ src/main/java/br/edu/ifrn/ifjobs/repository/RoleRepository.java
@@ -2,11 +2,14 @@ package br.edu.ifrn.ifjobs.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
 
 import br.edu.ifrn.ifjobs.model.Role;
+import br.edu.ifrn.ifjobs.model.enums.TipoUsuario;
 
+@Repository
 public interface RoleRepository extends JpaRepository<Role, Integer> {
 
     @Query("SELECT r FROM Role r WHERE r.nomeRole = ?1")
-    public Role findByNome(String nome);
+    public Role findByTipoUsuario(TipoUsuario nomeRole);
 }
--- src/main/java/br/edu/ifrn/ifjobs/repository/VagaRepository.java
+++ src/main/java/br/edu/ifrn/ifjobs/repository/VagaRepository.java
@@ -1,9 +1,11 @@
 package br.edu.ifrn.ifjobs.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import br.edu.ifrn.ifjobs.model.Vaga;
 
+@Repository
 public interface VagaRepository extends JpaRepository<Vaga, Integer> {
 
 }
