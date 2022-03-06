80ba125af56000b3f1e5bcd1fac63ff61416afee
Lucas Jdev <lucas.jdev1@gmail.com>
2022-03-06 18:14:02 -0300

    criação de exeções

---
+++ src/main/java/br/edu/ifrn/ifjobs/exception/VagaNaoCadastradaException.java
@@ -0,0 +1,8 @@
+package br.edu.ifrn.ifjobs.exception;
+
+public class VagaNaoCadastradaException extends Exception {
+
+    public VagaNaoCadastradaException(String mensagem) {
+        super(mensagem);
+    }
+}
---
+++ src/main/java/br/edu/ifrn/ifjobs/exception/VagaNaoEncontradoException.java
@@ -0,0 +1,8 @@
+package br.edu.ifrn.ifjobs.exception;
+
+public class VagaNaoEncontradoException extends Exception {
+
+    public VagaNaoEncontradoException(String mensagem) {
+        super(mensagem);
+    }
+}
