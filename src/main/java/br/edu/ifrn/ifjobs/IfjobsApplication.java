7d22f3396b7ff6d2d014049c1a72540f4b5d00c4
Lucas Jdev <lucas.jdev1@gmail.com>
2022-03-10 20:06:22 -0300

    habilitação de cache

--- src/main/java/br/edu/ifrn/ifjobs/IfjobsApplication.java
+++ src/main/java/br/edu/ifrn/ifjobs/IfjobsApplication.java
@@ -2,8 +2,10 @@ package br.edu.ifrn.ifjobs;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 
 @SpringBootApplication
+@EnableCaching
 public class IfjobsApplication {
 
 	public static void main(String[] args) {
