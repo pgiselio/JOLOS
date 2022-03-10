a10f99ec50a5745b76bb67f657f130de8db4e561
Lucas Jdev <lucas.jdev1@gmail.com>
2022-03-10 20:04:49 -0300

    erros para mensagens

---
+++ src/main/java/br/edu/ifrn/ifjobs/exception/MensagemNaoCadastradoException.java
@@ -0,0 +1,8 @@
+package br.edu.ifrn.ifjobs.exception;
+
+public class MensagemNaoCadastradoException extends Exception {
+
+    public MensagemNaoCadastradoException(String message) {
+        super(message);
+    }
+}
---
+++ src/main/java/br/edu/ifrn/ifjobs/exception/MensagemNaoEncontradaException.java
@@ -0,0 +1,8 @@
+package br.edu.ifrn.ifjobs.exception;
+
+public class MensagemNaoEncontradaException extends Exception {
+
+    public MensagemNaoEncontradaException(String message) {
+        super(message);
+    }
+}
