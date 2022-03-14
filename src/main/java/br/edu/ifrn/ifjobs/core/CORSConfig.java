853c21824e27a22cf0ef8ff57a35661216a01bcf
Lucas Jdev <lucas.jdev1@gmail.com>
2022-03-13 21:40:36 -0300

    configuração de cors

---
+++ src/main/java/br/edu/ifrn/ifjobs/core/CORSConfig.java
@@ -0,0 +1,17 @@
+package br.edu.ifrn.ifjobs.core;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CORSConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("https://ifjobs.heroku.app.com")
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS", "HEAD", "TRACE", "CONNECT");
+    }
+
+}
