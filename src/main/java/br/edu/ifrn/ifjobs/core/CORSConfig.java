6fd9856379d7fc6ffb925beb66434ebbcba274b2
Lucas Jdev <lucas.jdev1@gmail.com>
2022-03-15 18:11:41 -0300

    mais um teste

--- src/main/java/br/edu/ifrn/ifjobs/core/CORSConfig.java
+++ src/main/java/br/edu/ifrn/ifjobs/core/CORSConfig.java
@@ -1,17 +1,6 @@
 package br.edu.ifrn.ifjobs.core;
 
-import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-
-@Configuration
-public class CORSConfig implements WebMvcConfigurer {
-
-    @Override
-    public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**")
-                .allowedOrigins("http://localhost:3000")
-                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS", "HEAD", "TRACE", "CONNECT");
-    }
+// @Configuration
+public class CORSConfig {
 
 }
