2759ce52fa285321e91511fd9b82832f220b7424
Lucas Jdev <lucas.jdev1@gmail.com>
2022-03-15 17:55:33 -0300

    teste1

--- src/main/java/br/edu/ifrn/ifjobs/core/CORSConfig.java
+++ src/main/java/br/edu/ifrn/ifjobs/core/CORSConfig.java
@@ -1,10 +1,12 @@
 package br.edu.ifrn.ifjobs.core;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
+@EnableWebSecurity
 public class CORSConfig implements WebMvcConfigurer {
 
     @Override
