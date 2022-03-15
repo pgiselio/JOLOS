a6ed17f8e154cce554a5cef526c0720c59b52776
Lucas Jdev <lucas.jdev1@gmail.com>
2022-03-14 21:48:50 -0300

    refatoração dos métodos delete e atualização

--- src/main/java/br/edu/ifrn/ifjobs/service/UsuarioService.java
+++ src/main/java/br/edu/ifrn/ifjobs/service/UsuarioService.java
@@ -1,15 +1,19 @@
 package br.edu.ifrn.ifjobs.service;
 
+import java.lang.reflect.Field;
 import java.util.List;
+import java.util.Map;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.util.ReflectionUtils;
 
 import br.edu.ifrn.ifjobs.exception.UsuarioNaoCadastradoException;
 import br.edu.ifrn.ifjobs.exception.UsuarioNaoEncontradoException;
 import br.edu.ifrn.ifjobs.model.Role;
 import br.edu.ifrn.ifjobs.model.Usuario;
+import br.edu.ifrn.ifjobs.model.enums.StatusUsuario;
 import br.edu.ifrn.ifjobs.model.enums.TipoUsuario;
 import br.edu.ifrn.ifjobs.repository.RoleRepository;
 import br.edu.ifrn.ifjobs.repository.UsuarioRepository;
@@ -36,7 +40,7 @@ public class UsuarioService {
         return optional.orElseThrow(() -> new UsuarioNaoCadastradoException("Erro ao efetuar cadastro!"));
     }
 
-    public Usuario getById(int id) throws UsuarioNaoEncontradoException {
+    public Usuario buscaPorId(int id) throws UsuarioNaoEncontradoException {
         Optional<Usuario> usuarioFindById = usuarioRepository.findById(id);
         return usuarioFindById.orElseThrow(() -> new UsuarioNaoEncontradoException("Usuário não encontrado!!"));
     }
@@ -45,9 +49,29 @@ public class UsuarioService {
         return usuarioRepository.findAll();
     }
 
-    public Usuario delete(Usuario usuario) {
-        usuarioRepository.delete(usuario);
-        return usuario;
+    public Usuario atualizaCampos(int id, Map<Object, Object> campos)
+            throws UsuarioNaoEncontradoException, UsuarioNaoCadastradoException {
+        Usuario buscadoPorId = buscaPorId(id);
+
+        campos.forEach((chave, valor) -> {
+            Field campo = ReflectionUtils.findField(Usuario.class, (String) chave);
+            campo.setAccessible(true);
+            ReflectionUtils.setField(campo, buscadoPorId, valor);
+        });
+
+        return create(buscadoPorId);
+    }
+
+    public Usuario delete(Usuario usuario) throws Exception {
+        Optional<Usuario> usuarioOptional;
+        usuarioOptional = Optional.ofNullable(usuario);
+
+        usuarioOptional.ifPresent(user -> {
+            user.setStatus(StatusUsuario.DESATIVADO);
+            user = usuarioRepository.save(user);
+        });
+
+        return usuarioOptional.orElseThrow(Exception::new);
     }
 
 }
