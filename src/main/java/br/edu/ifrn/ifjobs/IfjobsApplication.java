ece93f680f87da3b7803c35c2a650cf27c673f65
Lucas Jdev <lucas.jdev1@gmail.com>
2022-03-15 20:01:10 -0300

    ativação de anotações de configurações necessárias

--- src/main/java/br/edu/ifrn/ifjobs/IfjobsApplication.java
+++ src/main/java/br/edu/ifrn/ifjobs/IfjobsApplication.java
@@ -3,9 +3,11 @@ package br.edu.ifrn.ifjobs;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 @SpringBootApplication
 @EnableCaching
+@EnableWebMvc
 public class IfjobsApplication {
 
 	public static void main(String[] args) {
