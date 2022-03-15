2570f729266f5a899dd38d9c87d532510ddd1bdf
Pedro Gisélio <pgiselio@hotmail.com>
2022-03-15 17:51:30 -0300

    teste 2

--- src/services/api.ts
+++ src/services/api.ts
@@ -5,16 +5,16 @@ export const Api = axios.create({
     baseURL: "https://ifjobs-backend.herokuapp.com/"
 });
 
-Api.interceptors.request.use(
-    (config) =>{
-        const user = getUserLocalStorage();
+// Api.interceptors.request.use(
+//     (config) =>{
+//         const user = getUserLocalStorage();
         
-        config.headers = {
-            Authorization: user?.token,
-        };
-        return config;
-    },
-    (error) =>{
-        return Promise.reject(error);
-    }
-) 
+//         config.headers = {
+//             Authorization: user?.token,
+//         };
+//         return config;
+//     },
+//     (error) =>{
+//         return Promise.reject(error);
+//     }
+// ) 
