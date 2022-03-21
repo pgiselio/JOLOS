58b0384cc3eeaaa442ba18b06fcf202d31be50c3
Lucas Jdev <lucas.jdev1@gmail.com>
2022-03-21 16:29:08 -0300

    criação de insert no db

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -17,5 +17,8 @@ VALUES ('jolos@jolos.com', '$2a$10$Cm9aPsc23ugEI7uqcGLPz.L89GVU2wlkKE1ddOJA6O76I
 INSERT INTO usuario (email, senha, status, empresa_id)
 VALUES ('java@jolos.com', '123jolos', 'CONCLUIDO', 1);
 
+INSERT INTO usuario (email, senha, status)
+VALUES ('Tjolinho@jolos.com', '$2a$10$Cm9aPsc23ugEI7uqcGLPz.L89GVU2wlkKE1ddOJA6O76IxEGo9wHG', 'PENDENTE')
+
 INSERT INTO vaga (curso_alvo, titulo, descricao, localizacao)
 VALUES ('Informática', 'Fullstack developer', 'você vai crescer assim como o software', 'João Câmara - RN')
