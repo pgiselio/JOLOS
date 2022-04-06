767b71d94b2559549052f9ea16f4052cd0e0fbb8
Lucas Jdev <lucas.jdev1@gmail.com>
2022-04-06 19:30:29 -0300

    criação de endpoints para curriculo

--- src/main/java/br/edu/ifrn/ifjobs/controller/CurriculoController.java
+++ src/main/java/br/edu/ifrn/ifjobs/controller/CurriculoController.java
@@ -5,15 +5,18 @@ import java.io.IOException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.io.ByteArrayResource;
 import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.server.ResponseStatusException;
 
 import br.edu.ifrn.ifjobs.exception.CurriculoNaoEncontradoException;
 import br.edu.ifrn.ifjobs.model.Arquivo;
@@ -27,6 +30,26 @@ public class CurriculoController {
     @Autowired
     private CurriculoService service;
 
+    @PostMapping("/{link}")
+    public ResponseEntity<Curriculo> uploadLink(@PathVariable(name = "link") String link) {
+        Curriculo curriculo = service.uploadLink(link);
+        return ResponseEntity.ok(curriculo);
+    }
+
+    @PatchMapping("/{id}/{link}")
+    public ResponseEntity<Curriculo> atualizaLink(@PathVariable(name = "id") int id,
+            @PathVariable(name = "link") String link) {
+        Curriculo curriculo;
+
+        try {
+            curriculo = service.atualizaLink(id, link);
+        } catch (CurriculoNaoEncontradoException e) {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, e.getMessage());
+        }
+
+        return ResponseEntity.ok(curriculo);
+    }
+
     @PostMapping(path = "/upload")
     public void upload(@RequestParam MultipartFile arquivo) {
         try {
@@ -37,7 +60,7 @@ public class CurriculoController {
     }
 
     @GetMapping("/download/{id}")
-    public ResponseEntity<?> buscaPorId(@PathVariable(name = "id") int id) {
+    public ResponseEntity<?> downloadPorId(@PathVariable(name = "id") int id) {
         Curriculo curriculo;
 
         try {
