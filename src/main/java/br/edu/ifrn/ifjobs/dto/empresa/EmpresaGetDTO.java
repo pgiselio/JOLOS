80c2302b1eafc37faef48284fe7e578be28d0ffb
Lucas Jdev <lucas.jdev1@gmail.com>
2022-04-10 15:06:21 -0300

    criação de getDto para empresa

---
+++ src/main/java/br/edu/ifrn/ifjobs/dto/empresa/EmpresaGetDTO.java
@@ -0,0 +1,87 @@
+package br.edu.ifrn.ifjobs.dto.empresa;
+
+import br.edu.ifrn.ifjobs.model.Pessoa;
+
+public class EmpresaGetDTO {
+
+    private int id;
+
+    private Pessoa dadosPessoa;
+
+    private String cnpj;
+
+    private String resumo;
+
+    private String telefone;
+
+    /**
+     * @return int return the id
+     */
+    public int getId() {
+        return id;
+    }
+
+    /**
+     * @param id the id to set
+     */
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    /**
+     * @return Pessoa return the dadosPessoa
+     */
+    public Pessoa getDadosPessoa() {
+        return dadosPessoa;
+    }
+
+    /**
+     * @param dadosPessoa the dadosPessoa to set
+     */
+    public void setDadosPessoa(Pessoa dadosPessoa) {
+        this.dadosPessoa = dadosPessoa;
+    }
+
+    /**
+     * @return String return the cnpj
+     */
+    public String getCnpj() {
+        return cnpj;
+    }
+
+    /**
+     * @param cnpj the cnpj to set
+     */
+    public void setCnpj(String cnpj) {
+        this.cnpj = cnpj;
+    }
+
+    /**
+     * @return String return the resumo
+     */
+    public String getResumo() {
+        return resumo;
+    }
+
+    /**
+     * @param resumo the resumo to set
+     */
+    public void setResumo(String resumo) {
+        this.resumo = resumo;
+    }
+
+    /**
+     * @return String return the telefone
+     */
+    public String getTelefone() {
+        return telefone;
+    }
+
+    /**
+     * @param telefone the telefone to set
+     */
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+
+}
