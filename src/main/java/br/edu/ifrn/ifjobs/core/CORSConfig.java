86b0edd9d43ca484b8ecc0d88e2e24e0a489feb5
Lucas Jdev <lucas.jdev1@gmail.com>
2022-04-10 23:50:02 -0300

    teste de impl para autorização só para a vercel

--- src/main/java/br/edu/ifrn/ifjobs/core/CORSConfig.java
+++ src/main/java/br/edu/ifrn/ifjobs/core/CORSConfig.java
@@ -14,7 +14,7 @@ public class CORSConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("*")
+                        .allowedOrigins("https://ifjobs.vercel.app")
                         .allowedMethods("*");
             }
         };
