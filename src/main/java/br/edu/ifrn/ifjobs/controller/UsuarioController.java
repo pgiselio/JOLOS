93a778a3677a93a92f45fd2d42f3f29c99b2c861
Lucas Jdev <lucas.jdev1@gmail.com>
2022-04-12 23:45:20 -0300

    cacheamento de usuário por email

--- src/main/java/br/edu/ifrn/ifjobs/controller/UsuarioController.java
+++ src/main/java/br/edu/ifrn/ifjobs/controller/UsuarioController.java
@@ -6,6 +6,8 @@ import java.util.Map;
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cache.annotation.CacheEvict;
+import org.springframework.cache.annotation.CachePut;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
@@ -52,6 +54,8 @@ public class UsuarioController {
     }
 
     @GetMapping("/email/{email}")
+    @CacheEvict(value = "usuarioEmail", allEntries = true)
+    @CachePut(value = "usuarioEmail")
     public ResponseEntity<UsuarioGetDTO> buscaPorEmail(@PathVariable(name = "email") String email) {
         Usuario usuario;
 
