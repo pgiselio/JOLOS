78d461a7bae55b492d2425b9bc09e4e3932fb3c7
Lucas Jdev <lucas.jdev1@gmail.com>
2022-04-13 23:15:58 -0300

    criação da classe redesSociais

---
+++ src/main/java/br/edu/ifrn/ifjobs/model/RedesSociais.java
@@ -0,0 +1,57 @@
+package br.edu.ifrn.ifjobs.model;
+
+import java.io.Serializable;
+
+import javax.persistence.Column;
+import javax.persistence.Embeddable;
+
+@Embeddable
+public class RedesSociais implements Serializable {
+
+    private static final long serialVersionUID = 123113212312411L;
+
+    @Column(length = 300)
+    private String linkedin;
+
+    @Column(length = 300)
+    private String instagram;
+
+    @Column(length = 300)
+    private String facebook;
+
+    @Column(length = 300)
+    private String twitter;
+
+    public String getLinkedin() {
+        return linkedin;
+    }
+
+    public void setLinkedin(String linkedin) {
+        this.linkedin = linkedin;
+    }
+
+    public String getIntagram() {
+        return instagram;
+    }
+
+    public void setIntagram(String instagram) {
+        this.instagram = instagram;
+    }
+
+    public String getFacebook() {
+        return facebook;
+    }
+
+    public void setFacebook(String facebook) {
+        this.facebook = facebook;
+    }
+
+    public String getTwitter() {
+        return twitter;
+    }
+
+    public void setTwitter(String twitter) {
+        this.twitter = twitter;
+    }
+
+}
