d0e70051f295069527eb066153a5106751887cc4
Lucas Jdev <lucas.jdev1@gmail.com>
2022-04-21 23:22:20 -0300

    ativação de email asincrono

--- src/main/java/br/edu/ifrn/ifjobs/IfjobsApplication.java
+++ src/main/java/br/edu/ifrn/ifjobs/IfjobsApplication.java
@@ -3,10 +3,12 @@ package br.edu.ifrn.ifjobs;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 @SpringBootApplication
 @EnableCaching
+@EnableAsync
 @EnableWebMvc
 public class IfjobsApplication {
 
