0ed81a3c49cf9f44145340e4b90f881d9e81cd77
Lucas Jdev <lucas.jdev1@gmail.com>
2022-05-08 14:59:32 -0300

    retirando anotação de descerialização

--- src/main/java/br/edu/ifrn/ifjobs/model/enums/StatusUsuario.java
+++ src/main/java/br/edu/ifrn/ifjobs/model/enums/StatusUsuario.java
@@ -4,7 +4,6 @@ import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
 
 import br.edu.ifrn.ifjobs.model.Usuario;
 
-@JsonDeserialize(as = Usuario.class)
 public enum StatusUsuario {
 
     PENDENTE, CONCLUIDO, DESATIVADO
