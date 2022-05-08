3c3ca42ab7863859b11d5e5215ed905cce338a5e
Lucas Jdev <lucas.jdev1@gmail.com>
2022-05-08 15:44:12 -0300

    criador de descerializador de autoridades

---
+++ src/main/java/br/edu/ifrn/ifjobs/model/DescerializadorDeAutoridades.java
@@ -0,0 +1,31 @@
+package br.edu.ifrn.ifjobs.model;
+
+import java.io.IOException;
+import java.util.Iterator;
+import java.util.LinkedList;
+
+import com.fasterxml.jackson.core.JsonParser;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.DeserializationContext;
+import com.fasterxml.jackson.databind.JsonDeserializer;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+
+public class DescerializadorDeAutoridades extends JsonDeserializer {
+
+    @Override
+    public Object deserialize(JsonParser p, DeserializationContext ctxt) throws IOException, JsonProcessingException {
+        ObjectMapper mapper = (ObjectMapper) p.getCodec();
+        JsonNode jsonNode = mapper.readTree(p);
+        LinkedList<SimpleGrantedAuthority> grantedAuthorities = new LinkedList<>();
+        Iterator<JsonNode> elements = jsonNode.elements();
+        while (elements.hasNext()) {
+            JsonNode next = elements.next();
+            JsonNode authority = next.get("authority");
+            grantedAuthorities.add(new SimpleGrantedAuthority(authority.asText()));
+        }
+        return grantedAuthorities;
+    }
+}
