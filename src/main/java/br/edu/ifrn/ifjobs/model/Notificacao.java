594bf70ec1dc6ff1eeb06139a7475368e15f2210
Lucas Jdev <lucas.jdev1@gmail.com>
2022-05-08 01:35:07 -0300

    criação da entidade de notificação

---
+++ src/main/java/br/edu/ifrn/ifjobs/model/Notificacao.java
@@ -0,0 +1,68 @@
+package br.edu.ifrn.ifjobs.model;
+
+import java.time.LocalDateTime;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Notificacao {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @Column(nullable = false)
+    private String titulo;
+    private String descricao;
+
+    @Column(nullable = false, columnDefinition = "TIMESTAMP")
+    private LocalDateTime data;
+
+    @Column(nullable = false)
+    private boolean visualizado;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public LocalDateTime getData() {
+        return data;
+    }
+
+    public void setData(LocalDateTime data) {
+        this.data = data;
+    }
+
+    public boolean isVisualizado() {
+        return visualizado;
+    }
+
+    public void setVisualizado(boolean visualizado) {
+        this.visualizado = visualizado;
+    }
+
+}
