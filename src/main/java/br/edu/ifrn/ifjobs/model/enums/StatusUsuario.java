5f1685d70b8ba1a8daae3163dd3bede6cbdd2b30
Lucas Jdev <lucas.jdev1@gmail.com>
2022-05-08 14:42:09 -0300

    teste de descerialização

--- src/main/java/br/edu/ifrn/ifjobs/model/enums/StatusUsuario.java
+++ src/main/java/br/edu/ifrn/ifjobs/model/enums/StatusUsuario.java
@@ -1,5 +1,10 @@
 package br.edu.ifrn.ifjobs.model.enums;
 
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+
+import br.edu.ifrn.ifjobs.model.Usuario;
+
+@JsonDeserialize(as = Usuario.class)
 public enum StatusUsuario {
 
     PENDENTE, CONCLUIDO, DESATIVADO
