64e62e0730a8e155c22b222a410361698fe37189
Lucas Jdev <lucas.jdev1@gmail.com>
2022-05-08 01:32:14 -0300

    ativação do scheduling

--- src/main/java/br/edu/ifrn/ifjobs/IfjobsApplication.java
+++ src/main/java/br/edu/ifrn/ifjobs/IfjobsApplication.java
@@ -4,11 +4,13 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 @SpringBootApplication
 @EnableCaching
 @EnableAsync
+@EnableScheduling
 @EnableWebMvc
 public class IfjobsApplication {
 
