d9243d9ee1ae78862c37d6846c68e820856e864d
Lucas Jdev <lucas.jdev1@gmail.com>
2022-05-29 18:59:04 -0300

    incrementação na lógica de busca notificacoes por email

--- src/main/java/br/edu/ifrn/ifjobs/controller/NotificacaoController.java
+++ src/main/java/br/edu/ifrn/ifjobs/controller/NotificacaoController.java
@@ -1,14 +1,18 @@
 package br.edu.ifrn.ifjobs.controller;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
+import br.edu.ifrn.ifjobs.dto.DTOConversor;
+import br.edu.ifrn.ifjobs.dto.notificacao.NotificacaoGetDTO;
 import br.edu.ifrn.ifjobs.model.Notificacao;
 import br.edu.ifrn.ifjobs.service.NotificacaoService;
 
@@ -20,12 +24,19 @@ public class NotificacaoController {
     private NotificacaoService notificacaoService;
 
     @GetMapping("/usuario/{email}")
-    public ResponseEntity<List<Notificacao>> buscaPorEmail(String email) {
-        return ResponseEntity.ok(notificacaoService.buscaTodasNaoVisualizadasBaseadaNoEmail(email));
+    public ResponseEntity<List<NotificacaoGetDTO>> buscaPorEmail(@PathVariable(name = "email") String email) {
+        List<Notificacao> notificacoesNaoVisualizadas;
+        notificacoesNaoVisualizadas = notificacaoService.buscaTodasNaoVisualizadasBaseadaNoEmail(email);
+
+        List<NotificacaoGetDTO> notificacoesDTO = notificacoesNaoVisualizadas.stream()
+                .map(notificacao -> DTOConversor.convertEntityToDto(notificacao, NotificacaoGetDTO.class))
+                .collect(Collectors.toList());
+
+        return ResponseEntity.ok(notificacoesDTO);
     }
 
     @GetMapping("/visualizar/{id}")
-    public ResponseEntity<Notificacao> visualizar(@PathVariable(name = "id") int id) {
+    public ResponseEntity<ResponseStatus> visualizar(@PathVariable(name = "id") int id) {
         notificacaoService.marcaComoVisualizada(id);
         return ResponseEntity.ok().build();
     }
