9cfed7fff3c66e46e4e3f74dcd597c2c6cb73970
Lucas Jdev <lucas.jdev1@gmail.com>
2022-05-29 22:35:20 -0300

    retirada de importação e criação de construtor

--- src/main/java/br/edu/ifrn/ifjobs/service/GeradorTokenService.java
+++ src/main/java/br/edu/ifrn/ifjobs/service/GeradorTokenService.java
@@ -2,7 +2,6 @@ package br.edu.ifrn.ifjobs.service;
 
 import java.util.Date;
 
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 import io.jsonwebtoken.Jwts;
@@ -12,6 +11,9 @@ import io.jsonwebtoken.SignatureAlgorithm;
 public class GeradorTokenService {
 
     private static String SECRET_KEY_JWT = "SEnHaDiFIciL1";
+
+    private GeradorTokenService() {
+    }
 
     public static String geraToken(String email, int tempoExpiracao) {
         final String JWT = Jwts.builder()
