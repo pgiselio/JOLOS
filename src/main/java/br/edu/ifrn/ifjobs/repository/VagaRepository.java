dec5149e072fdd5258dd30d2d8a7353407369b3f
Lucas Jdev <lucas.jdev1@gmail.com>
2022-05-30 22:43:23 -0300

    specification para repository

--- src/main/java/br/edu/ifrn/ifjobs/repository/VagaRepository.java
+++ src/main/java/br/edu/ifrn/ifjobs/repository/VagaRepository.java
@@ -1,11 +1,12 @@
 package br.edu.ifrn.ifjobs.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.stereotype.Repository;
 
 import br.edu.ifrn.ifjobs.model.Vaga;
 
 @Repository
-public interface VagaRepository extends JpaRepository<Vaga, Integer> {
+public interface VagaRepository extends JpaRepository<Vaga, Integer>, JpaSpecificationExecutor<Vaga> {
 
 }
