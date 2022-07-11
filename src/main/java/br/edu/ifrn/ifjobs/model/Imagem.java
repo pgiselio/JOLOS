7c11f443527a7bc27c97f326535786a57cfa64b4
Lucas Jdev <lucas.jdev1@gmail.com>
2022-07-10 21:25:12 -0300

    anotação table para a entidade imagem

--- src/main/java/br/edu/ifrn/ifjobs/model/Imagem.java
+++ src/main/java/br/edu/ifrn/ifjobs/model/Imagem.java
@@ -7,10 +7,12 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.Table;
 
 import org.springframework.stereotype.Component;
 
 @Entity
+@Table(name = "imagem")
 @Component
 public class Imagem implements Serializable {
 
