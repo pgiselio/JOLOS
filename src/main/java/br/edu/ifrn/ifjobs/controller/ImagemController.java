261102248365856856ab1186ce673bafdeff189b
Lucas Jdev <lucas.jdev1@gmail.com>
2022-07-17 10:34:51 -0300

    retirada de importação desnecessária

--- src/main/java/br/edu/ifrn/ifjobs/controller/ImagemController.java
+++ src/main/java/br/edu/ifrn/ifjobs/controller/ImagemController.java
@@ -12,7 +12,6 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
 import br.edu.ifrn.ifjobs.exception.UsuarioNaoCadastradoException;
-import br.edu.ifrn.ifjobs.model.Imagem;
 import br.edu.ifrn.ifjobs.service.ImagemService;
 
 @RestController
