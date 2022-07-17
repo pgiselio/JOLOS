c4bbdf33c7abc66ddb1e628b47dbe2be010868ec
Lucas Jdev <lucas.jdev1@gmail.com>
2022-07-17 16:06:06 -0300

    criação de enpoint

--- src/main/java/br/edu/ifrn/ifjobs/controller/ImagemController.java
+++ src/main/java/br/edu/ifrn/ifjobs/controller/ImagemController.java
@@ -4,6 +4,7 @@ import java.io.IOException;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -12,6 +13,7 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
 import br.edu.ifrn.ifjobs.exception.UsuarioNaoCadastradoException;
+import br.edu.ifrn.ifjobs.model.Imagem;
 import br.edu.ifrn.ifjobs.service.ImagemService;
 
 @RestController
@@ -32,6 +34,17 @@ public class ImagemController {
             ResponseEntity.status(417);
         }
         return ResponseEntity.ok().build();
+    }
+
+    @GetMapping("/{idUsuario}/fotoPerfil")
+    public ResponseEntity<?> getFotoPerfil(@PathVariable(name = "idUsuario") int idUsuario) {
+        var fotoPerfil = new Imagem();
+        try {
+            fotoPerfil = imagemService.getFotoPerfil(idUsuario);
+        } catch (UsuarioNaoCadastradoException e) {
+            ResponseEntity.status(404).body(e);
+        }
+        return ResponseEntity.ok(fotoPerfil);
     }
 
 }
