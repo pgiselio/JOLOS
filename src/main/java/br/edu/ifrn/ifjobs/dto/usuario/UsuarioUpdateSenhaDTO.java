848a2b07275400c5c8109323d8f82acaf8aa1981
Lucas Jdev <lucas.jdev1@gmail.com>
2022-08-06 21:44:59 -0300

    adição do campo token ao dto

--- src/main/java/br/edu/ifrn/ifjobs/dto/usuario/UsuarioUpdateSenhaDTO.java
+++ src/main/java/br/edu/ifrn/ifjobs/dto/usuario/UsuarioUpdateSenhaDTO.java
@@ -3,12 +3,21 @@ package br.edu.ifrn.ifjobs.dto.usuario;
 public class UsuarioUpdateSenhaDTO {
     
     private String senha;
-    
+    private String token;
+
     public String getSenha() {
         return senha;
     }
     
     public void setSenha(String senha) {
         this.senha = senha;
+    }
+
+    public String getToken() {
+        return token;
+    }
+
+    public void setToken(String token) {
+        this.token = token;
     }
 }
