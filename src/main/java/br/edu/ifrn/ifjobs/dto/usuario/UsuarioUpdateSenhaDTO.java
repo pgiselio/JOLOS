befda281bf7bcfad170aa1554f9305b7b7a82a78
Lucas Jdev <lucas.jdev1@gmail.com>
2022-08-06 21:15:40 -0300

    dto para senha

---
+++ src/main/java/br/edu/ifrn/ifjobs/dto/usuario/UsuarioUpdateSenhaDTO.java
@@ -0,0 +1,14 @@
+package br.edu.ifrn.ifjobs.dto.usuario;
+
+public class UsuarioUpdateSenhaDTO {
+    
+    private String senha;
+    
+    public String getSenha() {
+        return senha;
+    }
+    
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
+}
